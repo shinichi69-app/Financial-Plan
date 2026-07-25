c4eee2a4eba5c01e01bfb47f112f47b7a206230e
shinichi69-app <popsteptep555@gmail.com>
2026-07-25 20:31:00 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+﻿const CACHE_NAME = 'expense-tracker-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdn.tailwindcss.com',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
